7248080c303056a60a3787b0554aa42d97b601e0
Mmiller44 <Mmiller44@yahoo.com>
2014-02-23 19:51:15 -0500

    added rankings controller.

---
+++ webbies/app/scripts/controllers/rankingsController.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var App = angular.module('webbiesApp');
+App.controller('rankingsController', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+
+	}]);
